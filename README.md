f4b76488892faeddfefb77bfb234aba144260cf8
John Vasconcelos <johnvasconcelos7@gmail.com>
2013-11-19 00:32:57 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+     #Ruby on Rails Tutorials: first applications
+
+     This is the first application
+
+     [*Ruby on Rails Tutorail by John vasconcelos*]
+
+     (http://railstutorial.com/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '61da40211ceea68458099170ae459011ab41ea486b34a1d756b3ac635d159b4cdcde12d398a6cae7529929c31f6639fe377500ec2a2045622727c2194aa679a8'
